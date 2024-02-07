6783f5af8be12facd11611e715205979562c05fb
amna <amnamh2000@gmail.com>
2024-02-07 19:54:54 +0300

    task4

--- tasks.js
+++ tasks.js
@@ -29,6 +29,33 @@ function printEvenNumbersWithFor() {
 
 // Task 4
 function getDayOfWeek() {
+  var a;
+  switch (a) {
+    case 1:
+      console.log('sunday');
+      break;
+  case 2:
+    console.log('monday');
+    break;
+    case 3: 
+    console.log('tuesday');
+    break;
+    case 4:
+      console.log('wednesday');
+      break;
+    case 5:
+      console.log('thursday');
+      break;
+    case 6:
+      console.log('friday');
+      break;
+    case 7:
+      console.log('saturday');
+      break;
+    default:
+      console.log('new week');
+      break;
+  }
 
 }
 
